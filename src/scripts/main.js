4f9449da3525cbf8fbffae53821b8230ea1b4bab
iLyhaha <gollpfd@gmail.com>
2026-02-22 14:40:20 +0200

    Final Build

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
+/* eslint-disable prettier/prettier */
 'use strict';
+
+const burgerMenu = document.querySelector('.burger');
+const burgerButtonOpen = document.querySelector(
+  '.header__buttons-burger-button',
+);
+const burgerButtonClose = document.querySelector(
+  '.header__buttons-burger-button-open',
+);
+const burgerLinks = document.querySelectorAll('.burger__nav-link');
+
+burgerButtonOpen.addEventListener('click', () => {
+  burgerMenu.classList.add('open');
+  document.body.classList.add('body--lock');
+});
+
+burgerButtonClose.addEventListener('click', () => {
+  burgerMenu.classList.remove('open');
+  document.body.classList.remove('body--lock');
+});
+
+burgerLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    burgerMenu.classList.remove('open');
+    document.body.classList.remove('body--lock');
+  });
+});
